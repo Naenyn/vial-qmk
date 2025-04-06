b5d20af0407112ffdca3d535a7e5bbb6cf899051
Bill Smith <naenyn@gmail.com>
2025-04-06 02:57:37 -0400

    Cleanup, added CC14.

--- keyboards/binepad/kn01/keymaps/naenyn/keymap.c
+++ keyboards/binepad/kn01/keymaps/naenyn/keymap.c
@@ -2,9 +2,6 @@
 /* SPDX-License-Identifier: GPL-2.0-or-later */
 
 #include QMK_KEYBOARD_H
-// #include "analog.h"
-// #include "qmk_midi.h"
-// #include "quantum/midi/midi_device.h"
 extern MidiDevice midi_device;
 extern midi_config_t midi_config;
 
@@ -13,17 +10,16 @@ enum {
     _L1
 } keyboard_layers;
 
-// !! Enum keys here, repeat in vial.json
 enum naenyn_keycode {
     N_CC1_UP = QK_KB_0,
-    N_CC1_DN
+    N_CC1_DN,
+    N_CC14
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_L0] = LAYOUT_ortho_1x1(
         LT(_L1, KC_MUTE)
     ),
-
     [_L1] = LAYOUT_ortho_1x1(
         _______
     )
@@ -33,16 +29,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
     [_L0] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
-    [_L1] = { ENCODER_CCW_CW(N_CC1_DN, N_CC1_UP) }  // !! <-- MAP KEYS HERE, OR IN VIAL
+    [_L1] = { ENCODER_CCW_CW(N_CC1_DN, N_CC1_UP) }
 };
 
 #endif
 
-
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 	switch (keycode) {
 
-        // !! Each jey gets own functionality
 		case N_CC1_UP:
             if (record->event.pressed) {
                 midi_send_cc(&midi_device, midi_config.channel, 1, 65);
@@ -51,6 +45,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case N_CC1_DN:
             if (record->event.pressed) {
                 midi_send_cc(&midi_device, midi_config.channel, 1, 63);
+            }
+            return false;
+        case N_CC14:
+            if (record->event.pressed) {
+                midi_send_cc(&midi_device, midi_config.channel, 14, 64);
             }
             return false;
 	}
